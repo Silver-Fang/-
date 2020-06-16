8047ff86694e8d921a9117995924b5af613f1236
帕菲菲 <Leenrung@outlook.com>
2020-06-16 08:04:49 +0800

    添加项目文件。

---
+++ 从文件提取图像资源/MainPage.xaml.vb
@@ -0,0 +1,166 @@
+﻿Imports Windows.Storage
+''' <summary>
+''' 可用于自身或导航至 Frame 内部的空白页。
+''' </summary>
+Public NotInheritable Class MainPage
+    Inherits Page
+
+	ReadOnly 漫游设置 As IPropertySet = ApplicationData.Current.RoamingSettings.Values, 最近使用的列表 As AccessCache.StorageItemMostRecentlyUsedList = AccessCache.StorageApplicationPermissions.MostRecentlyUsedList, 文件选取器 As New Pickers.FileOpenPicker, 目录选取器 As New Pickers.FolderPicker
+	WithEvents 获取器 As New 从文件获取图像
+	Private 输入文件 As StorageFile, 输出目录 As StorageFolder
+
+	Private Sub JPG_Checked(sender As Object, e As RoutedEventArgs) Handles JPG.Checked, JPG.Unchecked
+		漫游设置("JPG") = JPG.IsChecked
+	End Sub
+	Private Sub PNG_Checked(sender As Object, e As RoutedEventArgs) Handles PNG.Checked, PNG.Unchecked
+		漫游设置("PNG") = PNG.IsChecked
+	End Sub
+	Private Sub SWF_Checked(sender As Object, e As RoutedEventArgs) Handles SWF.Checked, SWF.Unchecked
+		漫游设置("SWF") = SWF.IsChecked
+	End Sub
+	Private Async Sub 异步初始化()
+		If 最近使用的列表.ContainsItem("输入文件") Then
+			输入文件 = Await 最近使用的列表.GetFileAsync("输入文件")
+			If 输入文件 IsNot Nothing Then 输入路径.Text = 输入文件.Path
+		End If
+		If 最近使用的列表.ContainsItem("输出目录") Then
+			输出目录 = Await 最近使用的列表.GetFolderAsync("输出目录")
+			If 输出目录 IsNot Nothing Then 输出路径.Text = 输出目录.Path
+		End If
+	End Sub
+	Sub New()
+
+		' 此调用是设计器所必需的。
+		InitializeComponent()
+
+		' 在 InitializeComponent() 调用之后添加任何初始化。
+
+		If 漫游设置.ContainsKey("JPG") Then JPG.IsChecked = 漫游设置("JPG")
+		If 漫游设置.ContainsKey("PNG") Then PNG.IsChecked = 漫游设置("PNG")
+		If 漫游设置.ContainsKey("SWF") Then SWF.IsChecked = 漫游设置("SWF")
+		If 漫游设置.ContainsKey("优化") Then
+			Select Case 漫游设置("优化")
+				Case 优化.内存
+					内存优化.IsChecked = True
+				Case 优化.速度
+					速度优化.IsChecked = True
+			End Select
+		End If
+		文件选取器.FileTypeFilter.Add("*")
+		目录选取器.FileTypeFilter.Add("*")
+		If 漫游设置.ContainsKey("起始字节") Then 起始字节.Text = 漫游设置("起始字节")
+		异步初始化()
+	End Sub
+	Enum 优化 As Byte
+		速度
+		内存
+	End Enum
+	Private Sub 内存优化_Checked(sender As Object, e As RoutedEventArgs) Handles 内存优化.Checked
+		漫游设置("优化") = CByte(优化.内存)
+	End Sub
+
+	Private Sub 速度优化_Checked(sender As Object, e As RoutedEventArgs) Handles 速度优化.Checked
+		漫游设置("优化") = CByte(优化.速度)
+	End Sub
+
+	Private Async Sub 输入浏览_Click(sender As Object, e As RoutedEventArgs) Handles 输入浏览.Click
+		输入文件 = Await 文件选取器.PickSingleFileAsync
+		If 输入文件 IsNot Nothing Then
+			最近使用的列表.AddOrReplace("输入文件", 输入文件)
+			输入路径.Text = 输入文件.Path
+		End If
+	End Sub
+
+	Private Async Sub 输出浏览_Click(sender As Object, e As RoutedEventArgs) Handles 输出浏览.Click
+		输出目录 = Await 目录选取器.PickSingleFolderAsync
+		If 输出目录 IsNot Nothing Then
+			最近使用的列表.AddOrReplace("输出目录", 输出目录)
+			输出路径.Text = 输出目录.Path
+		End If
+	End Sub
+
+	ReadOnly 进度(3) As Integer
+	Private Async Sub 开始搜索_Click(sender As Object, e As RoutedEventArgs) Handles 开始搜索.Click
+		If 输入文件 Is Nothing Then
+			提示信息.Text = "未指定输入文件"
+			浮出窗口.ShowAt(sender)
+			Exit Sub
+		End If
+		If 输出目录 Is Nothing Then
+			提示信息.Text = "未指定输出目录"
+			浮出窗口.ShowAt(sender)
+			Exit Sub
+		End If
+		If 内存优化.IsChecked = False AndAlso 速度优化.IsChecked = False Then
+			提示信息.Text = "未指定优化策略"
+			浮出窗口.ShowAt(sender)
+			Exit Sub
+		End If
+		If PNG.IsChecked = False AndAlso JPG.IsChecked = False AndAlso SWF.IsChecked = False Then
+			提示信息.Text = "未指定待查类型"
+			浮出窗口.ShowAt(sender)
+			Exit Sub
+		End If
+		Dim b As UInteger
+		Try
+			b = 起始字节.Text
+		Catch ex As Exception
+			提示信息.Text = "无效的起始字节"
+			浮出窗口.ShowAt(sender)
+			Exit Sub
+		End Try
+		Dim a As Stream = Await 输入文件.OpenStreamForReadAsync, f As UInteger = a.Length - b, c(f - 1) As Byte
+		a.Position = b
+		Dim d As Task = a.ReadAsync(c, 0, f)
+		漫游设置("起始字节") = b
+		开始搜索.IsEnabled = False
+		内存优化.IsEnabled = False
+		速度优化.IsEnabled = False
+		输入浏览.IsEnabled = False
+		输出浏览.IsEnabled = False
+		PNG.IsEnabled = False
+		JPG.IsEnabled = False
+		SWF.IsEnabled = False
+		进度.Initialize()
+		If 内存优化.IsChecked Then
+			进度条.Maximum = f
+			进度条.Value = 0
+			Await d
+			Await 获取器.开始获取_内存优化(c.ToList, PNG.IsChecked, JPG.IsChecked, SWF.IsChecked, b)
+		End If
+		If 速度优化.IsChecked Then
+			进度条.Maximum = f * (If(PNG.IsChecked, 1, 0) + If(JPG.IsChecked, 1, 0) + If(SWF.IsChecked, 1, 0))
+			进度条.Value = 0
+			Await d
+			Await 获取器.开始获取_速度优化(c, PNG.IsChecked, JPG.IsChecked, SWF.IsChecked, b)
+		End If
+		开始搜索.IsEnabled = True
+		内存优化.IsEnabled = True
+		速度优化.IsEnabled = True
+		输入浏览.IsEnabled = True
+		输出浏览.IsEnabled = True
+		PNG.IsEnabled = True
+		JPG.IsEnabled = True
+		SWF.IsEnabled = True
+	End Sub
+
+	Private Sub 获取器_进度报告(已检查字节数 As UInteger, 总字节数 As UInteger, 类型 As 文件类型) Handles 获取器.进度报告
+		进度(类型) = 已检查字节数
+		Call Dispatcher.TryRunIdleAsync(Sub() 进度条.Value = 进度.Sum())
+	End Sub
+
+	Private Async Sub 获取器_找到文件(文件 As IEnumerable(Of Byte), 类型 As 文件类型, 起始字节 As UInteger, 长度 As UInteger) Handles 获取器.找到文件
+		Dim a As String = ""
+		Select Case 类型
+			Case 文件类型.PNG
+				a = ".png"
+			Case 文件类型.JPG
+				a = ".jpg"
+			Case 文件类型.SWF
+				a = ".swf"
+		End Select
+		Dim b As Stream = Await (Await 输出目录.CreateFileAsync(起始字节 & a, CreationCollisionOption.ReplaceExisting)).OpenStreamForWriteAsync
+		b.Write(文件.ToArray, 0, 长度)
+		b.Close()
+	End Sub
+End Class
---
+++ 从文件提取图像资源/从文件获取图像.vb
@@ -0,0 +1,171 @@
+﻿Public Enum 文件类型 As Byte
+	PNG = 0
+	JPG = 1
+	SWF = 2
+	所有 = 3
+End Enum
+Public Class 从文件获取图像
+	Const PNG标头长度 As Byte = 16, PNG末尾长度 As Byte = 8, JPG标头长度 As Byte = 8, JPG末尾长度 As Byte = 2, SWF标头长度 As Byte = 3
+	Private Shared Function 字节流转字符串(字节流 As Byte()) As String
+		Static ChrW委托 As Func(Of Byte, Char) = AddressOf ChrW
+		Dim a As IEnumerable(Of Char) = 字节流.Select(ChrW委托), b As Char(), c As New Text.StringBuilder
+		Const 枚举最大长度 As UInteger = 536870912
+		Do
+			b = a.Take(枚举最大长度).ToArray
+			a = a.Skip(枚举最大长度)
+			c.Append(b)
+		Loop While b.Length = 枚举最大长度
+		Return c.ToString
+	End Function
+
+	Event 找到文件(文件 As IEnumerable(Of Byte), 类型 As 文件类型, 起始字节 As UInteger, 长度 As UInteger)
+	Event 进度报告(已检查字节数 As UInteger, 总字节数 As UInteger, 类型 As 文件类型)
+	Private Sub 获取PNG(源 As ArraySegment(Of Byte), 字符串 As String, 当前已检查的大小 As Integer)
+		Const PNG至少长度 As Byte = 24
+		Static PNG标头 As String = 字节流转字符串({&H89, &H50, &H4E, &H47, &HD, &HA, &H1A, &HA, &H0, &H0, &H0, &HD, &H49, &H48, &H44, &H52}), PNG末尾 As String = 字节流转字符串({&H49, &H45, &H4E, &H44, &HAE, &H42, &H60, &H82})
+		Dim b As UInteger = 字符串.Length, PNG最后位置 As UInteger = b - PNG至少长度, 开始 As Integer, 结束 As Integer, 文件长度 As UInteger
+		While 当前已检查的大小 < PNG最后位置
+			RaiseEvent 进度报告(当前已检查的大小, b, 文件类型.PNG)
+			开始 = 字符串.IndexOf(PNG标头, 当前已检查的大小, StringComparison.Ordinal)
+			If 开始 = -1 Then Exit While
+			结束 = 字符串.IndexOf(PNG末尾, 开始 + PNG标头长度, StringComparison.Ordinal)
+			If 结束 = -1 Then Exit While
+			当前已检查的大小 = 结束 + PNG末尾长度
+			文件长度 = 当前已检查的大小 - 开始
+			RaiseEvent 找到文件(源.Skip(开始).Take(文件长度), 文件类型.PNG, 开始, 文件长度)
+		End While
+		RaiseEvent 进度报告(b, b, 文件类型.PNG)
+	End Sub
+	Private Sub 获取JPG(源 As ArraySegment(Of Byte), 字符串 As String, 当前已检查的大小 As Integer)
+		Const JPG至少长度 As Byte = 24
+		Static JPG标头 As String = 字节流转字符串({&HFF, &HD8, &HFF, &HE0, &H0, &H10, &H4A, &H46}), JPG末尾 As String = 字节流转字符串({&HFF, &HD9})
+		Dim b As UInteger = 字符串.Length, JPG最后位置 As UInteger = b - JPG至少长度, 开始 As Integer, 结束 As Integer, 文件长度 As UInteger
+		While 当前已检查的大小 < JPG最后位置
+			RaiseEvent 进度报告(当前已检查的大小, b, 文件类型.JPG)
+			开始 = 字符串.IndexOf(JPG标头, 当前已检查的大小, StringComparison.Ordinal)
+			If 开始 = -1 Then Exit While
+			结束 = 字符串.IndexOf(JPG末尾, 开始 + JPG标头长度, StringComparison.Ordinal)
+			If 结束 = -1 Then Exit While
+			当前已检查的大小 = 结束 + JPG末尾长度
+			文件长度 = 当前已检查的大小 - 开始
+			RaiseEvent 找到文件(源.Skip(开始).Take(文件长度), 文件类型.JPG, 开始, 文件长度)
+		End While
+		RaiseEvent 进度报告(b, b, 文件类型.JPG)
+	End Sub
+	Private Sub 获取SWF(源 As Byte(), 字符串 As String, 当前已检查的大小 As Integer)
+		Const SWF至少长度 As Byte = 8
+		Dim b As UInteger = 字符串.Length, SWF最后位置 As UInteger = b - SWF至少长度, 开始 As Integer, c As UInteger, 文件长度 As UInteger
+		Static SWF标头 As String = 字节流转字符串({&H46, &H57, &H53})
+		While 当前已检查的大小 < SWF最后位置
+			RaiseEvent 进度报告(当前已检查的大小, b, 文件类型.SWF)
+			开始 = 字符串.IndexOf(SWF标头, 当前已检查的大小, StringComparison.Ordinal)
+			If 开始 = -1 Then Exit While
+			c = 开始 + SWF标头长度
+			If 源(c) < 20 Then
+				文件长度 = New IO.BinaryReader(New IO.MemoryStream(源.Skip(c + 1).Take(4).ToArray)).ReadUInt32
+				If 开始 + 文件长度 > b Then
+					当前已检查的大小 = 开始 + 1
+				Else
+					RaiseEvent 找到文件(源.Skip(开始).Take(文件长度), 文件类型.SWF, 开始, 文件长度)
+					当前已检查的大小 = 开始 + 文件长度
+				End If
+			Else
+				当前已检查的大小 = 开始 + 1
+			End If
+		End While
+		RaiseEvent 进度报告(b, b, 文件类型.SWF)
+	End Sub
+	Function 开始获取_速度优化(源 As Byte(), 提取PNG As Boolean, 提取JPG As Boolean, 提取SWF As Boolean, Optional 起始字节 As UInteger = 0) As Task
+		Return Task.Run(Async Function()
+							Dim a As String = 字节流转字符串(源), 任务列表 As New List(Of Task)
+							If 提取PNG Then
+								任务列表.Add(Task.Run(Sub()
+													  获取PNG(源, a, 起始字节)
+												  End Sub))
+							End If
+							If 提取JPG Then
+								任务列表.Add(Task.Run(Sub()
+													  获取JPG(源, a, 起始字节)
+												  End Sub))
+							End If
+							If 提取SWF Then
+								任务列表.Add(Task.Run(Sub()
+													  获取SWF(源, a, 起始字节)
+												  End Sub))
+							End If
+							For Each b As Task In 任务列表
+								Await b
+							Next
+						End Function)
+	End Function
+	Function 开始获取_内存优化(源 As List(Of Byte), 提取PNG As Boolean, 提取JPG As Boolean, 提取SWF As Boolean, Optional 起始字节 As UInteger = 0) As Task
+		Return Task.Run(Sub() 开始获取_内存优化_内部(源, 提取PNG, 提取JPG, 提取SWF, 起始字节))
+	End Function
+	Private Sub 开始获取_内存优化_内部(源 As List(Of Byte), 提取PNG As Boolean, 提取JPG As Boolean, 提取SWF As Boolean, 起始字节 As UInteger)
+		Dim 启发标头 As New List(Of Byte)
+		If 提取PNG Then 启发标头.Add(&H89)
+		If 提取JPG Then 启发标头.Add(&HFF)
+		If 提取SWF Then 启发标头.Add(&H46)
+		Dim 是启发标头 As Predicate(Of Byte) = AddressOf 启发标头.Contains
+		Static PNG标头 As Byte() = {&H89, &H50, &H4E, &H47, &HD, &HA, &H1A, &HA, &H0, &H0, &H0, &HD, &H49, &H48, &H44, &H52}, PNG末尾 As Byte() = {&H49, &H45, &H4E, &H44, &HAE, &H42, &H60, &H82}, JPG标头 As Byte() = {&HFF, &HD8, &HFF, &HE0, &H0, &H10, &H4A, &H46}, JPG末尾 As Byte() = {&HFF, &HD9}, SWF标头 As Byte() = {&H46, &H57, &H53}, 是PNG末尾启发 As Predicate(Of Byte) = AddressOf &H49.Equals, 是JPG末尾启发 As Predicate(Of Byte) = AddressOf &HFF.Equals, 标头类型 As New Dictionary(Of Byte, Byte) From {{&H89, 文件类型.PNG}, {&HFF, 文件类型.JPG}, {&H46, 文件类型.SWF}}
+		Dim 开始 As UInteger, a As UInteger, b As UInteger = 源.Count, c As UInteger, d As UInteger = b - 7, e As UInteger
+		While 起始字节 < d
+			RaiseEvent 进度报告(起始字节, b, 文件类型.所有)
+			开始 = 源.FindIndex(起始字节, 是启发标头)
+			If 开始 = -1 Then Exit While
+			Select Case 标头类型(源(开始))
+				Case 文件类型.PNG
+					If 源.Skip(开始).Take(PNG标头长度).SequenceEqual(PNG标头) Then
+						a = 开始 + PNG标头长度
+						While a < b
+							c = 源.FindIndex(a, 是PNG末尾启发)
+							If c = -1 Then Exit While
+							If 源.Skip(c).Take(PNG末尾长度).SequenceEqual(PNG末尾) Then
+								起始字节 = c + PNG末尾长度
+								e = 起始字节 - 开始
+								RaiseEvent 找到文件(源.Skip(开始).Take(e), 文件类型.PNG, 开始, e)
+								Exit Select
+							Else
+								a = c + 1
+							End If
+						End While
+					End If
+					起始字节 = 开始 + 1
+				Case 文件类型.JPG
+					If 源.Skip(开始).Take(JPG标头长度).SequenceEqual(JPG标头) Then
+						a = 开始 + JPG标头长度
+						While a < b
+							c = 源.FindIndex(a, 是JPG末尾启发)
+							If c = -1 Then Exit While
+							If 源.Skip(c).Take(JPG末尾长度).SequenceEqual(JPG末尾) Then
+								起始字节 = c + JPG末尾长度
+								e = 起始字节 - 开始
+								RaiseEvent 找到文件(源.Skip(开始).Take(e), 文件类型.JPG, 开始, e)
+								Exit Select
+							Else
+								a = c + 1
+							End If
+						End While
+					End If
+					起始字节 = 开始 + 1
+				Case 文件类型.SWF
+					If 源.Skip(开始).Take(SWF标头长度).SequenceEqual(SWF标头) Then
+						a = 开始 + SWF标头长度
+						If 源(a) < 20 Then
+							e = New IO.BinaryReader(New IO.MemoryStream(源.Skip(a + 1).Take(4).ToArray)).ReadUInt32
+							起始字节 = 开始 + e
+							If 起始字节 > b Then
+								起始字节 = 开始 + 1
+							Else
+								RaiseEvent 找到文件(源.Skip(开始).Take(e), 文件类型.SWF, 开始, e)
+							End If
+						Else
+							起始字节 = 开始 + 1
+						End If
+					Else
+						起始字节 = 开始 + 1
+					End If
+			End Select
+		End While
+	End Sub
+End Class
